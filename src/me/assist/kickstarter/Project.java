308614e366a8e96ad7ed346a5530fe8dc951cb4b
Assist <assist@laptop.home>
2014-06-22 17:11:39 +0300

    Save project funders in Project class

--- src/me/assist/kickstarter/Project.java
+++ src/me/assist/kickstarter/Project.java
@@ -1,5 +1,7 @@
 package me.assist.kickstarter;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -15,12 +17,25 @@ public class Project {
 	private double target;
 	private double collected;
 	
+	private Map<String, Double> funders;
+	
 	public Project(String owner, UUID id, String name, double target, double collected) {
 		this.owner = owner;
 		this.id = id;
 		this.name = name;
 		this.target = target;
 		this.collected = collected;
+		
+		funders = new HashMap<>();
+		// need to somehow load existing funders
+	}
+	
+	public void addFunder(String name, double amount) {
+		funders.put(name, amount);
+	}
+	
+	public Map<String, Double> getFunders() {
+		return funders;
 	}
 
 	public String getPlayer() {
