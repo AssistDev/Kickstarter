a18bfefe123f486a661bb5bcf29fc1bf2774c0aa
AssistDev <avastprods@gmail.com>
2014-06-23 14:54:48 +0300

    Load existing funders

--- src/me/assist/kickstarter/Project.java
+++ src/me/assist/kickstarter/Project.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-
 public class Project {
 
+	private Kickstarter plugin;
 
 	private UUID owner;
 	private String name;
@@ -15,14 +15,35 @@ public class Project {
 
 	private Map<String, Double> funders;
 
-	public Project(UUID owner, String name, double target, double collected) {
+	public Project(Kickstarter instance, UUID owner, String name, double target, double collected) {
+		plugin = instance;
+
 		this.owner = owner;
 		this.name = name;
 		this.target = target;
 		this.collected = collected;
 
 		funders = new HashMap<>();
-		
+		loadFunders();
+	}
+
+	private void loadFunders() {
+		if (plugin.getConfiguration().contains(getName() + ".funders")) {
+			for (String s : plugin.getConfiguration().getStringList(getName() + ".funders")) {
+				String[] r = s.split(":");
+				String n = r[0];
+				double k = 0;
+
+				try {
+					k = Double.parseDouble(r[1]);
+				} catch (NumberFormatException ex) {
+					ex.printStackTrace();
+					return;
+				}
+				
+				funders.put(n,  k);
+			}
+		}
 	}
 
 	public void addFunder(String name, double amount) {
