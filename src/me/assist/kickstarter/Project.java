9e73aa063310f9fb234918c8acb24dd3410d2de6
AssistDev <avastprods@gmail.com>
2014-06-23 14:43:43 +0300

    Switched from player names to UUIDs (c2)

--- src/me/assist/kickstarter/Project.java
+++ src/me/assist/kickstarter/Project.java
@@ -7,53 +7,44 @@ import java.util.UUID;
 
 public class Project {
 
-	/*
-	 * Required for OfflinePlayer economy deposit
-	 */
-	private UUID id;
-	
-	private String owner;
+
+	private UUID owner;
 	private String name;
 	private double target;
 	private double collected;
-	
+
 	private Map<String, Double> funders;
-	
-	public Project(String owner, UUID id, String name, double target, double collected) {
+
+	public Project(UUID owner, String name, double target, double collected) {
 		this.owner = owner;
-		this.id = id;
 		this.name = name;
 		this.target = target;
 		this.collected = collected;
-		
+
 		funders = new HashMap<>();
-		// need to somehow load existing funders
+		
 	}
-	
+
 	public void addFunder(String name, double amount) {
 		funders.put(name, amount);
 	}
-	
+
 	public Map<String, Double> getFunders() {
 		return funders;
 	}
 
-	public String getPlayer() {
+	public UUID getOwner() {
 		return owner;
-	}
-	
-	public UUID getPlayerUUID() {
-		return id;
 	}
 
 	public String getName() {
 		return name;
 	}
-	
+
 	public double getTarget() {
 		return target;
 	}
-	
+
 	public double getCollected() {
 		return collected;
 	}
