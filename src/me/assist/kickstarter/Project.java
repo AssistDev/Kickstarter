bb577afcacbac8946c9556be167a7965c36d27a4
AssistDev <avastprods@gmail.com>
2014-06-26 11:34:28 +0300

    Revive from file loss (c2)

--- src/me/assist/kickstarter/Project.java
+++ src/me/assist/kickstarter/Project.java
@@ -5,35 +5,33 @@ import java.util.Map;
 import java.util.UUID;
 
 public class Project {
-
 	private Kickstarter plugin;
 
 	private UUID owner;
 	private String name;
 	private double target;
 	private double collected;
-
 	private Map<String, Double> funders;
 
 	public Project(Kickstarter instance, UUID owner, String name, double target, double collected) {
-		plugin = instance;
+		this.plugin = instance;
 
 		this.owner = owner;
 		this.name = name;
 		this.target = target;
 		this.collected = collected;
 
-		funders = new HashMap<>();
+		this.funders = new HashMap<>();
 		loadFunders();
 	}
 
 	private void loadFunders() {
-		if (plugin.getConfiguration().contains(getName() + ".funders")) {
-			for (String s : plugin.getConfiguration().getStringList(getName() + ".funders")) {
+		if (this.plugin.getConfiguration().contains(getOwner().toString() + ".funders")) {
+			for (String s : this.plugin.getConfiguration().getStringList(getOwner().toString() + ".funders")) {
 				String[] r = s.split(":");
 				String n = r[0];
-				double k = 0;
-
+				double k = 0.0D;
+				
 				try {
 					k = Double.parseDouble(r[1]);
 				} catch (NumberFormatException ex) {
@@ -41,32 +39,32 @@ public class Project {
 					return;
 				}
 				
-				funders.put(n,  k);
+				this.funders.put(n, k);
 			}
 		}
 	}
 
 	public void addFunder(String name, double amount) {
-		funders.put(name, amount);
+		this.funders.put(name, amount);
 	}
 
 	public Map<String, Double> getFunders() {
-		return funders;
+		return this.funders;
 	}
 
 	public UUID getOwner() {
-		return owner;
+		return this.owner;
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public double getTarget() {
-		return target;
+		return this.target;
 	}
 
 	public double getCollected() {
-		return collected;
+		return this.collected;
 	}
 }
